900899dc6069bdf524956160cafae2e915b5d1a0
BassemSadaqah <bassem.sadaqah@gmail.com>
2020-05-12 22:12:01 +0200

    frontend

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var pdf2 = require('./pdf2.js');
 global.hummus = require('hummus');
 var app = express();
 app.use(express.static('public'));
-pdf2('tst.pdf',[/a/g,'f'])
-pdf.replaceText('tst.pdf', 'output.pdf', 1, ['a', 'p', 'l', 'e'], ['а', 'р', 'ӏ', 'е']);
-app.get('*',(req,res)=>{
+//pdf2('tst.pdf',[/a/g,'f'])
+//pdf.replaceText('tst.pdf', 'output.pdf', 1, ['a', 'p', 'l', 'e'], ['а', 'р', 'ӏ', 'е']);
+
+app.get('/',(req,res)=>{
     res.send('x')
 })
 
