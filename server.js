9033e499cc69334b36fac92cc7850d8bdcda5500
fadysadaqah <fadysadaqah@gmail.com>
2020-05-14 20:29:42 +0200

    responsivity updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var pdf = require('./pdf.js');
 var pdf2 = require('./pdf2.js');
-global.hummus = require('hummus');
+//global.hummus = require('hummus');
 var app = express();
 app.use(express.static('public'));
 //pdf2('tst.pdf',[/a/g,'f'])
